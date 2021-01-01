4fda559c9771ef0f76c982d30dfd4d3ba5638a45
kyoseki <76502906+kyoseki@users.noreply.github.com>
2021-01-01 10:34:11 -0800

    Fix test capitalization

--- kyoseki.Game.Tests/Visual/TestScenekyosekiGame.cs
+++ kyoseki.Game.Tests/Visual/TestScenekyosekiGame.cs
@@ -4,7 +4,7 @@ using osu.Framework.Testing;
 
 namespace kyoseki.Game.Tests.Visual
 {
-    public class TestScenekyosekiGame : TestScene
+    public class TestSceneKyosekiGame : TestScene
     {
         // Add visual tests to ensure correct behaviour of your game: https://github.com/ppy/osu-framework/wiki/Development-and-Testing
         // You can make changes to classes associated with the tests and they will recompile and update immediately.
