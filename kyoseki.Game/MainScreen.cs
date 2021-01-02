729c67332b36d8531437b08c1d06e2ece1bf4718
kyoseki <76502906+kyoseki@users.noreply.github.com>
2021-01-01 19:07:01 -0800

    Remove blue box from main screen

--- kyoseki.Game/MainScreen.cs
+++ kyoseki.Game/MainScreen.cs
@@ -3,7 +3,6 @@ using kyoseki.Game.Overlays;
 using kyoseki.Game.UI.SerialMonitor;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Screens;
 
@@ -22,11 +21,7 @@ namespace kyoseki.Game
                 {
                     RelativeSizeAxes = Axes.Both
                 },
-                new Box
                 {
-                    Colour = Colour4.Blue,
-                    Size = new osuTK.Vector2(5, 5)
-                },
                 new BasicButton
                 {
                     AutoSizeAxes = Axes.Both,
