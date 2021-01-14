327486d32d3b1934f3733e8dd58ec1d5cb05637b
kyoseki <76502906+kyoseki@users.noreply.github.com>
2021-01-13 20:53:48 -0800

    Create extension method for converting quaternion to euler

---
+++ kyoseki.Game/MathUtils/QuaternionExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Numerics;
+
+namespace kyoseki.Game.MathUtils
+{
+    public static class QuaternionExtensions
+    {
+        // https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        // TODO: a better function may exist
+        public static (float x, float y, float z) ToEuler(this Quaternion q, bool degrees = true)
+        {
+            float sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
+            float cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
+            float roll = MathF.Atan2(sinr_cosp, cosr_cosp);
+
+            float sinp = 2 * (q.W * q.Y - q.Z * q.X);
+            float pitch;
+            if (MathF.Abs(sinp) >= 1)
+                pitch = MathF.PI / 2 * (sinp < 0 ? -1 : 1);
+            else
+                pitch = MathF.Asin(sinp);
+
+            float siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
+            float cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z + q.Z);
+            float yaw = MathF.Atan2(siny_cosp, cosy_cosp);
+
+            if (degrees)
+            {
+                roll *= 180 / MathF.PI;
+                pitch *= 180 / MathF.PI;
+                yaw *= 180 / MathF.PI;
+            }
+
+            return (roll, pitch, yaw);
+        }
+    }
+}
