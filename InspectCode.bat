aeff08ae2672ae42bb2f93449a720172068c7cba
kyoseki <76502906+kyoseki@users.noreply.github.com>
2021-01-14 14:45:01 -0800

    Add dotnet format

--- InspectCode.bat
+++ InspectCode.bat
@@ -1,4 +1,5 @@
 dotnet tool restore
+dotnet format --check
 dotnet jb inspectcode kyoseki.Desktop.slnf --output="inspectcodereport.xml" --caches-home="inspectcode" --verbosity=INFO
 dotnet nvika parsereport "inspectcodereport.xml" --treatwarningsaserrors
 pause
