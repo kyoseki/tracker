b5e99c423391ba5a388fda289acd1b727e69f70b
kyoseki <76502906+kyoseki@users.noreply.github.com>
2021-01-14 23:06:12 -0800

    Rework SkeletonLinkManager to manage links for each port

--- kyoseki.Game/Serial/SkeletonLinkManager.cs
+++ kyoseki.Game/Serial/SkeletonLinkManager.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Numerics;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
+using osu.Framework.Threading;
 
 namespace kyoseki.Game.Serial
 {
@@ -13,16 +15,87 @@ namespace kyoseki.Game.Serial
         [Resolved]
         private ConnectionManager serialConnections { get; set; }
 
-        private readonly List<SkeletonLink> links = new List<SkeletonLink>();
+        private readonly List<SkeletonSerialProcessor> ports = new List<SkeletonSerialProcessor>();
 
         // TODO: do better
         public readonly BindableList<int> ReceiverIds = new BindableList<int>();
+
+        public new ScheduledDelegate Schedule(Action action) => base.Schedule(action);
 
         protected override void LoadComplete()
         {
             base.LoadComplete();
 
+            serialConnections.PortsUpdated += handlePortsUpdated;
             serialConnections.MessageReceived += handleMessage;
+
+            serialConnections.PortNames.ForEach(p => registerPort(p));
+        }
+
+        public SkeletonSerialProcessor GetPort(string port) => ports.Find(p => p.Port == port);
+
+        public void Register(SkeletonLink link, string port)
+        {
+            lock (ports)
+            {
+                var oldPort = GetPort(link.Port);
+
+                oldPort?.Unregister(link);
+
+                var newPort = GetPort(port);
+
+                newPort.Register(link);
+                link.Port = port;
+            }
+        }
+
+        private SkeletonSerialProcessor registerPort(string port)
+        {
+            lock (ports)
+            {
+                var newPort = new SkeletonSerialProcessor(this, port);
+                ports.Add(newPort);
+                return newPort;
+            }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+
+            serialConnections.PortsUpdated -= handlePortsUpdated;
+            serialConnections.MessageReceived -= handleMessage;
+        }
+
+        private void handlePortsUpdated(string[] added, string[] removed)
+        {
+            added.ForEach(p => registerPort(p));
+        }
+
+        private void handleMessage(MessageInfo msg)
+        {
+            lock (ports)
+            {
+                var port = ports.Find(p => p.Port == msg.Port);
+                port?.Update(msg);
+            }
+        }
+    }
+
+    public class SkeletonSerialProcessor
+    {
+        public readonly string Port;
+
+        public readonly BindableList<int> ReceiverIds = new BindableList<int>();
+
+        private readonly List<SkeletonLink> links = new List<SkeletonLink>();
+
+        private readonly SkeletonLinkManager skeletonLinks;
+
+        public SkeletonSerialProcessor(SkeletonLinkManager skeletonLinks, string port)
+        {
+            this.skeletonLinks = skeletonLinks;
+            Port = port;
         }
 
         public void Register(SkeletonLink link)
@@ -37,37 +110,27 @@ namespace kyoseki.Game.Serial
 
         public void Unregister(SkeletonLink link) => links.Remove(link);
 
-        protected override void Dispose(bool isDisposing)
-        {
-            base.Dispose(isDisposing);
-
-            serialConnections.MessageReceived -= handleMessage;
-        }
-
-        private void handleMessage(MessageInfo msg)
+        public void Update(MessageInfo msg)
         {
             var split = msg.Content.Split(" ");
 
-            if (split.Length == 6)
+            if (int.TryParse(split[0], out int receiverId) &&
+                int.TryParse(split[1], out int sensorId) &&
+                float.TryParse(split[2], out float w) &&
+                float.TryParse(split[3], out float x) &&
+                float.TryParse(split[4], out float y) &&
+                float.TryParse(split[5], out float z))
             {
-                if (int.TryParse(split[0], out int receiverId) &&
-                    int.TryParse(split[1], out int sensorId) &&
-                    float.TryParse(split[2], out float w) &&
-                    float.TryParse(split[3], out float x) &&
-                    float.TryParse(split[4], out float y) &&
-                    float.TryParse(split[5], out float z))
+                Quaternion quat = new Quaternion(x, y, z, w);
+
+                skeletonLinks.Schedule(() =>
                 {
-                    Quaternion quat = new Quaternion(x, y, z, w);
+                    if (!ReceiverIds.Contains(receiverId))
+                        ReceiverIds.Add(receiverId);
+                });
 
-                    Schedule(() =>
-                    {
-                        if (!ReceiverIds.Contains(receiverId))
-                            ReceiverIds.Add(receiverId);
-                    });
-
-                    var link = links.Find(l => l.Port == msg.Port && l.ReceiverId == receiverId);
-                    link?.Update(new ReceiverMessage(sensorId, quat));
-                }
+                var link = links.Find(l => l.Port == Port && l.ReceiverId == receiverId);
+                link?.Update(new ReceiverMessage(sensorId, quat));
             }
         }
     }
